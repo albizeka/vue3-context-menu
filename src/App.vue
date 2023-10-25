639a5acf5adb97f00c9ad1e892860f553e07fd6a
Albi Zeka <albiizeka@gmail.com>
2023-10-25 16:52:22 +0200

    Update readme and sample file

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,28 @@
 <template>
   <div>
-
     <div>
-      <p @contextmenu.prevent="$refs.menu.open">
-          Right click on me
-      </p>    
-  </div>
-  
-  <vue-context ref="menu">
-      <li>
-          <a href="#" @click.prevent="onClick($event.target.innerText)">Option 1</a>
+      <p @contextmenu.prevent="$refs.menu.open">Right click on me</p>
+    </div>
+
+    <VueContext ref="menu">
+      <li v-for="item in items">
+        <a href="#" @click.prevent="onClick($event.target.innerText)">{{
+          item
+        }}</a>
       </li>
-      <li>
-          <a href="#" @click.prevent="onClick($event.target.innerText)">Option 2</a>
-      </li>
-  </vue-context>
+    </VueContext>
   </div>
 </template>
 
-<script>
-import VueContext from './js/index';
-import { ref, nextTick } from 'vue';
+<script setup>
+import VueContext from "./js/index";
+import { ref, nextTick } from "vue";
 
-export default {
-  components: { VueContext },
-  data() {
-    return {
-      close: true,
-      closeCount: 0,
-      items: [
-        'Cras justo odio',
-        'Dapibus ac facilisis in',
-        'Morbi leo risus',
-        'Porta ac consectetur ac',
-        'Vestibulum at eros'
-      ],
-    };
-  },
-  methods: {
-    async openMenu(event) {
-      // Wait until the DOM has been updated and refs are set
-      await nextTick();
-      
-      // Now you can safely access $refs.menu
-      if (this.$refs.menu) {
-        this.$refs.menu.open(event);
-      }
-    },
-  },
-};
+// Context items
+const items = ref(["Item 1", "Item 2"]);
+
+// Action
+function onClick(param) {
+  console.log("Item clicked");
+}
 </script>
