e1da8e05ae476baecdd4d8490e1d9c35b852db4f
Albi Zeka <albiizeka@gmail.com>
2023-10-25 12:29:34 +0200

    Change version of package

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/js/index.js'),
+      name: 'vue-context',
+      fileName: 'vue-context'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   plugins: [
     vue(),
     vueJsx(),
